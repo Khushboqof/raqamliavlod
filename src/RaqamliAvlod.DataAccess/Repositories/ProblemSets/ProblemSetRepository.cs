13cdcd45b3284293e0757b57f166f1563f440b5f
azizsayfullayev <azizsayfullayev1@gmail.com>
2022-10-26 23:52:27 +0500

    Created Reposiory of ProblemSets

---
+++ src/RaqamliAvlod.DataAccess/Repositories/ProblemSets/ProblemSetRepository.cs
@@ -0,0 +1,18 @@
+﻿using RaqamliAvlod.DataAccess.DbContexts;
+using RaqamliAvlod.DataAccess.Interfaces.ProblemSets;
+using RaqamliAvlod.Domain.Entities.ProblemSets;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RaqamliAvlod.DataAccess.Repositories.ProblemSets
+{
+    public class ProblemSetRepository : GenericRepository<ProblemSet>, IProblemSetRepository
+    {
+        public ProblemSetRepository(AppDbContext context) : base(context)
+        {
+        }
+    }
+}
