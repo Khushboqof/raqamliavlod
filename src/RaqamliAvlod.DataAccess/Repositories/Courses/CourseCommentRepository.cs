b40077fccbd8665f40bfb053cdababa9c67758d4
azizsayfullayev <azizsayfullayev1@gmail.com>
2022-10-26 23:51:59 +0500

    Created Repository of Courses

---
+++ src/RaqamliAvlod.DataAccess/Repositories/Courses/CourseCommentRepository.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Microsoft.EntityFrameworkCore.Query.Internal;
+using RaqamliAvlod.DataAccess.DbContexts;
+using RaqamliAvlod.DataAccess.Interfaces.Courses;
+using RaqamliAvlod.Domain.Entities.Courses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RaqamliAvlod.DataAccess.Repositories.Courses
+{
+    public class CourseCommentRepository : ICourseCommentRepository
+    {
+        private readonly AppDbContext _dbSet;
+
+        public CourseCommentRepository(AppDbContext context)
+        {
+            _dbSet = context;
+        }
+        public async Task<CourseComment> CreateAsync(CourseComment entity)
+        {
+            await _dbSet.CourseComments.AddAsync(entity);
+            await _dbSet.SaveChangesAsync();
+
+            return entity;
+
+        }
+
+        public async Task<CourseComment> DeleteAsync(long id)
+        {
+            var entity = await _dbSet.CourseComments.FindAsync(id);
+            if (entity is not null)
+            {
+                _dbSet.Remove(entity);
+                await _dbSet.SaveChangesAsync();
+                return entity;
+            }
+            else throw new NullReferenceException("Not found entity to remove");
+        }
+
+        public async Task<CourseComment?> FindByIdAsync(long id)
+        {
+            var entity = await _dbSet.CourseComments.FindAsync(id);
+            if (entity is not null)
+            {
+                return entity;
+            }
+            else throw new NullReferenceException("Not found entity to remove");
+        }
+
+        public async Task<CourseComment> UpdateAsync(long id, CourseComment entity)
+        {
+            var oldEntity = await _dbSet.CourseComments.FindAsync(id);
+            if (oldEntity is not null)
+            {
+                entity.Id = id;
+                _dbSet.CourseComments.Update(entity);
+                await _dbSet.SaveChangesAsync();
+                return entity;
+            }
+            else throw new NullReferenceException("Not found entity to update");
+        }
+    }
+}
---
+++ src/RaqamliAvlod.DataAccess/Repositories/Courses/CourseRepository.cs
@@ -0,0 +1,18 @@
+﻿using RaqamliAvlod.DataAccess.DbContexts;
+using RaqamliAvlod.DataAccess.Interfaces.Courses;
+using RaqamliAvlod.Domain.Entities.Courses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RaqamliAvlod.DataAccess.Repositories.Courses
+{
+    public class CourseRepository : GenericRepository<Course>, ICourseRepository
+    {
+        public CourseRepository(AppDbContext context) : base(context)
+        {
+        }
+    }
+}
