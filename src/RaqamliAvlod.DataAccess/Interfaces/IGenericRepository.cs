11721807c6caea1ebaba0f74cde165743c5c1fbf
SobirjonovSodiqjonovich <otkirbeksobirjonov@gmail.com>
2022-10-26 12:26:25 +0500

    created generic repository

---
+++ src/RaqamliAvlod.DataAccess/Interfaces/IGenericRepository.cs
@@ -0,0 +1,12 @@
+﻿using CodePower.DataAccess.Common.Interfaces;
+using RaqamliAvlod.DataAccess.Common.Interfaces;
+using RaqamliAvlod.Domain.Common;
+
+namespace RaqamliAvlod.DataAccess.Interfaces
+{
+    public interface IGenericRepository<T> : ICreateable<T>,
+        IReadable<T>, IUpdateable<T>, IFindable<T>, IDeleteable<T>
+        where T : BaseEntity
+    {
+    }
+}
