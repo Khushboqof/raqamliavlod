876f367dd6c992a7a015094cb9da94aa57a2561a
Kamronbek Maxamadzoidov <kamron25bek25@gmail.com>
2022-10-26 16:17:01 +0500

    User interface completed

---
+++ src/RaqamliAvlod.DataAccess/Interfaces/Users/IUserRepository.cs
@@ -0,0 +1,12 @@
+﻿using CodePower.DataAccess.Common.Interfaces;
+using RaqamliAvlod.DataAccess.Common.Interfaces;
+using RaqamliAvlod.Domain.Entities.Users;
+
+namespace RaqamliAvlod.DataAccess.Interfaces.Users
+{
+    public interface IUserRepository 
+        : ICreateable<User>, IReadable<User>, IFindable<User>, IUpdateable<User>
+    {
+
+    }
+}
