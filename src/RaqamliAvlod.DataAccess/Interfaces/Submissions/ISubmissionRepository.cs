6d7ed9a93336e52f63c2f695e64b492bd7c27bab
Kamronbek Maxamadzoidov <kamron25bek25@gmail.com>
2022-10-26 16:17:22 +0500

    Submission interface complated

---
+++ src/RaqamliAvlod.DataAccess/Interfaces/Submissions/ISubmissionRepository.cs
@@ -0,0 +1,11 @@
+﻿using CodePower.DataAccess.Common.Interfaces;
+using RaqamliAvlod.DataAccess.Common.Interfaces;
+using RaqamliAvlod.Domain.Entities.Submissions;
+
+namespace RaqamliAvlod.DataAccess.Interfaces.Submissions
+{
+    public interface ISubmissionRepository : ICreateable<Submission>, IFindable<Submission>, IReadable<Submission>
+    {
+
+    }
+}
