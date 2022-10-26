53b293ea3e0e5b9a38f48170827d3a02301f5d81
Kamronbek Maxamadzoidov <kamron25bek25@gmail.com>
2022-10-26 16:18:41 +0500

    ProblemSets interfaces section is complated

---
+++ src/RaqamliAvlod.DataAccess/Interfaces/ProblemSets/IProblemSetTestRepository.cs
@@ -0,0 +1,12 @@
+﻿using CodePower.DataAccess.Common.Interfaces;
+using RaqamliAvlod.DataAccess.Common.Interfaces;
+using RaqamliAvlod.Domain.Entities.ProblemSets;
+
+namespace RaqamliAvlod.DataAccess.Interfaces.ProblemSets
+{
+    public interface IProblemSetTestRepository
+        : ICreateable<ProblemSetTest>, IReadable<ProblemSetTest>, IUpdateable<ProblemSetTest>, IDeleteable<ProblemSetTest>
+    {
+
+    }
+}
