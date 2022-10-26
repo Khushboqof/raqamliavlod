3afcfee39745e9d577bf3d29d4aba882452aa461
azizsayfullayev <azizsayfullayev1@gmail.com>
2022-10-27 00:07:15 +0500

    Question repositories created

---
+++ src/RaqamliAvlod.DataAccess/Repositories/Questions/QuestionRepository.cs
@@ -0,0 +1,18 @@
+﻿using RaqamliAvlod.DataAccess.DbContexts;
+using RaqamliAvlod.DataAccess.Interfaces.Questions;
+using RaqamliAvlod.Domain.Entities.Questions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RaqamliAvlod.DataAccess.Repositories.Questions
+{
+    public class QuestionRepository : GenericRepository<Question>, IQuestionRepository
+    {
+        public QuestionRepository(AppDbContext context) : base(context)
+        {
+        }
+    }
+}
