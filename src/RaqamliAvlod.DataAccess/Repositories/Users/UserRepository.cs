f21e4495fcacd54e7effe747f2710963eb5b2522
Kamronbek Maxamadzoidov <kamron25bek25@gmail.com>
2022-10-27 14:48:59 +0500

    UserRepository updated

--- src/RaqamliAvlod.DataAccess/Repositories/Users/UserRepository.cs
+++ src/RaqamliAvlod.DataAccess/Repositories/Users/UserRepository.cs
@@ -1,9 +1,10 @@
 using RaqamliAvlod.DataAccess.DbContexts;
+using RaqamliAvlod.DataAccess.Interfaces.Users;
 using RaqamliAvlod.Domain.Entities.Users;
 
 namespace RaqamliAvlod.DataAccess.Repositories.Users
 {
-    public class UserRepository : GenericRepository<User>
+    public class UserRepository : GenericRepository<User>, IUserRepository
     {
         public UserRepository(AppDbContext context) : base(context)
         {
