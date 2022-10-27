0cc2cf544a5174af9028385fb353ea06400ac3d9
SobirjonovSodiqjonovich <otkirbeksobirjonov@gmail.com>
2022-10-27 11:12:56 +0500

    configured all repository interfaces

--- src/RaqamliAvlod.DataAccess/Interfaces/IGenericRepository.cs
+++ src/RaqamliAvlod.DataAccess/Interfaces/IGenericRepository.cs
@@ -1,11 +1,9 @@
-﻿using CodePower.DataAccess.Common.Interfaces;
-using RaqamliAvlod.DataAccess.Common.Interfaces;
+﻿using RaqamliAvlod.DataAccess.Common.Interfaces;
 using RaqamliAvlod.Domain.Common;
 
 namespace RaqamliAvlod.DataAccess.Interfaces
 {
-    public interface IGenericRepository<T> : ICreateable<T>,
-        IReadable<T>, IUpdateable<T>, IFindable<T>, IDeleteable<T>
+    public interface IGenericRepository<T> : IRepository<T>, IReadable<T>
         where T : BaseEntity
     {
     }
--- src/RaqamliAvlod.DataAccess/Interfaces/Users/IUserRepository.cs
+++ src/RaqamliAvlod.DataAccess/Interfaces/Users/IUserRepository.cs
@@ -1,11 +1,8 @@
-﻿using CodePower.DataAccess.Common.Interfaces;
-using RaqamliAvlod.DataAccess.Common.Interfaces;
-using RaqamliAvlod.Domain.Entities.Users;
+﻿using RaqamliAvlod.Domain.Entities.Users;
 
 namespace RaqamliAvlod.DataAccess.Interfaces.Users
 {
-    public interface IUserRepository 
-        : ICreateable<User>, IReadable<User>, IFindable<User>, IUpdateable<User>
+    public interface IUserRepository : IGenericRepository<User>
     {
 
     }
