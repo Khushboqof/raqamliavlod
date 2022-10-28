85b9e131d49adc80ecdad413b9f17a4d068b514e
Kamronbek Maxamadzoidov <kamron25bek25@gmail.com>
2022-10-28 10:11:30 +0500

    QuestionAnswerRepository is updated

--- src/RaqamliAvlod.DataAccess/Repositories/Questions/QuestionAnswerRepository.cs
+++ src/RaqamliAvlod.DataAccess/Repositories/Questions/QuestionAnswerRepository.cs
@@ -1,14 +1,22 @@
-﻿using RaqamliAvlod.DataAccess.DbContexts;
+﻿using CodePower.DataAccess.Common;
+using RaqamliAvlod.Application.Utils;
+using RaqamliAvlod.DataAccess.DbContexts;
 using RaqamliAvlod.DataAccess.Interfaces.Questions;
 using RaqamliAvlod.Domain.Entities.Questions;
 
-
 namespace RaqamliAvlod.DataAccess.Repositories.Questions
 {
-    public class QuestionAnswerRepository : GenericRepository<QuestionAnswer>, IQuestionAnswerRepository
+    public class QuestionAnswerRepository : BaseRepository<QuestionAnswer>, IQuestionAnswerRepository
     {
         public QuestionAnswerRepository(AppDbContext context) : base(context)
         {
+        }
+
+        public async Task<PagedList<QuestionAnswer>> GetAllByCourseIdAsync(long questionId, PaginationParams @params)
+        {
+            var questionAnswers = _dbSet.Where(answers => answers.QuestionId == questionId);
+
+            return await PagedList<QuestionAnswer>.ToPagedListAsync(questionAnswers, @params.PageNumber, @params.PageSize);
         }
     }
 }
