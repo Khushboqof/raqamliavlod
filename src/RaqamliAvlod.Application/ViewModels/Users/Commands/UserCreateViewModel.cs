09a195adea9e6b1d5456a0106950488b962489d5
Sukhrob Khayitmuradov <sergeytakhirov@gmail.com>
2022-10-28 18:06:39 +0500

    added UserCreateViewModel, Abbos aka should do this tbh -___-

---
+++ src/RaqamliAvlod.Application/ViewModels/Users/Commands/UserCreateViewModel.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RaqamliAvlod.Application.ViewModels.Users.Commands;
+
+public class UserCreateViewModel
+{
+    public string Firstname { get; set; } = string.Empty;
+    public string Lastname { get; set; } = string.Empty;
+    public string PhoneNumber { get; set; } = string.Empty;
+    public IFormFile? Image { get; set; }
+}
