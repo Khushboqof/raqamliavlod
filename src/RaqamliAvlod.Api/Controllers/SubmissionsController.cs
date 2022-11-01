172437cc969e6ea6e3e86023011b0bdb0c9724ca
Sukhrob Khayitmuradov <sergeytakhirov@gmail.com>
2022-11-01 11:40:11 +0500

    added submissions controller

--- src/RaqamliAvlod.Api/Controllers/SubmissionsController.cs
+++ src/RaqamliAvlod.Api/Controllers/SubmissionsController.cs
@@ -1,9 +1,27 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using RaqamliAvlod.Application.Utils;
 
 namespace RaqamliAvlod.Api.Controllers;
 
-[Route("api/[controller]")]
+[Route("api/submissions")]
 [ApiController]
 public class SubmissionsController : ControllerBase
 {
+    [HttpGet]
+    public async Task<IActionResult> GetAllAsync([FromBody] PaginationParams @params)
+    {
+        return Ok();
+    }
+
+    [HttpGet("{submissionId}")]
+    public async Task<IActionResult> GetAsync(long submissionId)
+    {
+        return Ok();
+    }
+
+    [HttpGet("{problemSetId}/{userId}")]
+    public async Task<IActionResult> GetSubmissionUserAsync(long problemsetId, long userId, [FromBody] PaginationParams @params)
+    {
+        return Ok();
+    }
 }
