e4edbe33c1721c37c1db235221a927f5132ca314
SobirjonovSodiqjonovich <otkirbeksobirjonov@gmail.com>
2022-11-01 11:09:32 +0500

    updated Controllers

--- src/RaqamliAvlod.Application/ViewModels/ProblemSets/Commands/ProblemSetCreateViewModel.cs
+++ src/RaqamliAvlod.Application/ViewModels/ProblemSets/Commands/ProblemSetCreateViewModel.cs
@@ -1,19 +1,40 @@
 ﻿using RaqamliAvlod.Domain.Entities.ProblemSets;
+using System.ComponentModel.DataAnnotations;
 
 namespace RaqamliAvlod.Application.ViewModels.ProblemSets.Commands
 {
     public class ProblemSetCreateViewModel
     {
+        [Required]
         public string Name { get; set; } = String.Empty;
+
+        [Required]
         public string Description { get; set; } = String.Empty;
+
+        [Required]
         public string Type { get; set; } = String.Empty;
+
+        public string Note { get; set; } = String.Empty;
+
+        [Required]
         public string InputDescription { get; set; } = String.Empty;
+
+        [Required]
         public string OutputDescription { get; set; } = String.Empty;
+
+        [Required]
         public int TimeLimit { get; set; }
+
+        [Required]
         public int MemoryLimit { get; set; }
+
+        [Required]
         public byte Difficulty { get; set; }
+
+        [Required]
         public bool IsPublic { get; set; }
 
+        [Required]
         public long OwnerId { get; set; }
 
         public long? ContestId { get; set; }
