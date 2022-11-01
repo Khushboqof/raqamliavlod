40989e564b7657baf9dc1c916347f33647eaefba
SobirjonovSodiqjonovich <otkirbeksobirjonov@gmail.com>
2022-11-01 10:27:30 +0500

    updated

--- src/RaqamliAvlod.Application/ViewModels/Submissions/Commands/ContestSubmissionCreateViewModel.cs
+++ src/RaqamliAvlod.Application/ViewModels/Submissions/Commands/ContestSubmissionCreateViewModel.cs
@@ -6,10 +6,13 @@ namespace RaqamliAvlod.Application.ViewModels.Submissions.Commands
 {
     public class ContestSubmissionCreateViewModel
     {
+        [Required]
         public long ContestId { get; set; }
         
+        [Required]
         public long ProblemSetId { get; set; }
         
+        [Required]
         public string Language { get; set; } = string.Empty;
         
         [Required]
