830ff55998a81bf1bd01cf6ca070549ef0838af7
Temur <ubaydullayev117@gmail.com>
2022-11-01 18:23:02 +0500

    Created Paginator Service Interface

---
+++ src/RaqamliAvlod.Infrastructure.Service/Interfaces/Common/IPaginatorService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RaqamliAvlod.Infrastructure.Service.Interfaces.Common
+{
+    public interface IPaginatorService
+    {
+    }
+}
