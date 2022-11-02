a0209de879fdfb356ca9fb35d4e4f675af7d96e1
Khushboqof <xushboqov.elbek@mail.ru>
2022-11-02 18:32:17 +0500

    The IEmailService interface is written in the Service layer's Inerface section

---
+++ src/RaqamliAvlod.Infrastructure.Service/Interfaces/Common/IEmailService.cs
@@ -0,0 +1,15 @@
+﻿using RaqamliAvlod.Application.ViewModels.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RaqamliAvlod.Infrastructure.Service.Interfaces.Common
+{
+    public interface IEmailService
+    {
+        public Task SendAsync(EmailMessage message);
+    }
+}
