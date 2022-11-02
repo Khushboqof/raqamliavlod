5b2fbe4240826ccd2ad908f1e146838724e7b70e
Temur <ubaydullayev117@gmail.com>
2022-11-02 07:39:20 +0500

    Added ToPagenator method in Paginator Service Interface

--- src/RaqamliAvlod.Infrastructure.Service/Interfaces/Common/IPaginatorService.cs
+++ src/RaqamliAvlod.Infrastructure.Service/Interfaces/Common/IPaginatorService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using RaqamliAvlod.Application.Utils;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,6 @@ namespace RaqamliAvlod.Infrastructure.Service.Interfaces.Common
 {
     public interface IPaginatorService
     {
+        Task ToPagenatorAsync(PaginationMetaData metaData);
     }
 }
