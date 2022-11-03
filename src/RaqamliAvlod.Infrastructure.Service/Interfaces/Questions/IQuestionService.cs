58c501a01f38cd7004ecead042682a6b4bbad53e
Sukhrob Khayitmuradov <sergeytakhirov@gmail.com>
2022-11-03 10:59:41 +0500

    pushed questionservice

---
+++ src/RaqamliAvlod.Infrastructure.Service/Interfaces/Questions/IQuestionService.cs
@@ -0,0 +1,18 @@
+﻿using RaqamliAvlod.Application.Utils;
+using RaqamliAvlod.Application.ViewModels.Questions.Commands;
+using RaqamliAvlod.Application.ViewModels.Questions.Queries;
+using RaqamliAvlod.Domain.Entities.Questions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RaqamliAvlod.Infrastructure.Service.Interfaces.Questions;
+
+public interface IQuestionService
+{
+    Task<QuestionViewModel> UpdateAsync(QuestionUpdateViewModel questionUpdateViewModel);
+    Task<bool> DeleteAsync(Expression<Func<Question, bool>> expression);
+}
