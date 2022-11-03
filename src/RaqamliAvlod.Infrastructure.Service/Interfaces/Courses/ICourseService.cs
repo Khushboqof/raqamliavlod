d6a8aa41792f2a53d63e3a7eb2963e62ef8da6d2
Temur <ubaydullayev117@gmail.com>
2022-11-03 14:52:39 +0500

    Created Course Service Interface and added new method

---
+++ src/RaqamliAvlod.Infrastructure.Service/Interfaces/Courses/ICourseService.cs
@@ -0,0 +1,19 @@
+﻿using RaqamliAvlod.Application.Utils;
+using RaqamliAvlod.Application.ViewModels.Courses;
+using RaqamliAvlod.Infrastructure.Service.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RaqamliAvlod.Infrastructure.Service.Interfaces.Courses;
+
+public interface ICourseService
+{
+    Task<bool> CreateAsync(long userId, CourseCreateDto dto);
+    Task<bool> UpdateAsync(long userId, CourseUpdateDto dto);
+    Task<bool> DeleteAsync(long id);
+    Task<CourseViewModel> GetAsync(long id);
+    Task<IEnumerable<CourseViewModel>> GetAllAsync(PaginationParams @params);
+}
