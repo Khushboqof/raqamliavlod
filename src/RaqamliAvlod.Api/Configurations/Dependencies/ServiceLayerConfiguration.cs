791873d54a9fc6cdb0f46edd1ceed00a60dcac25
Khushboqof <xushboqov.elbek@mail.ru>
2022-11-04 11:01:33 +0500

    Account Service connected controller

--- src/RaqamliAvlod.Api/Configurations/Dependencies/ServiceLayerConfiguration.cs
+++ src/RaqamliAvlod.Api/Configurations/Dependencies/ServiceLayerConfiguration.cs
@@ -1,10 +1,13 @@
-﻿namespace RaqamliAvlod.Api.Configurations.Dependencies
+﻿using RaqamliAvlod.Infrastructure.Service.Interfaces.Users;
+using RaqamliAvlod.Infrastructure.Service.Services.Users;
+
+namespace RaqamliAvlod.Api.Configurations.Dependencies
 {
     public static class ServiceLayerConfiguration
     {
         public static void AddServiceLayer(this WebApplicationBuilder builder)
         {
-
+            
         }
     }
 }
