dbd521389a037540496ea2e8718ff98b95c9b2eb
Temur <ubaydullayev117@gmail.com>
2022-11-07 10:03:26 +0500

    Course Service added new method

--- src/RaqamliAvlod.Infrastructure.Service/Interfaces/Courses/ICourseService.cs
+++ src/RaqamliAvlod.Infrastructure.Service/Interfaces/Courses/ICourseService.cs
@@ -6,9 +6,9 @@ namespace RaqamliAvlod.Infrastructure.Service.Interfaces.Courses;
 
 public interface ICourseService
 {
-    Task<bool> CreateAsync(long ownerId, CourseCreateDto dto);
-    Task<bool> UpdatePutAsync(long ownerId, CourseUpdateDto dto);
-    Task<bool> UpdatePatchAsync(long ownerId, CourseUpdateDto dto);
+    Task<bool> CreateAsync(CourseCreateDto dto);
+    Task<bool> UpdatePutAsync(long courseId, CourseUpdateDto dto);
+    Task<bool> UpdatePatchAsync(long courseId, CourseUpdateDto dto);
     Task<bool> DeleteAsync(long id);
     Task<CourseViewModel> GetAsync(long id);
     Task<IEnumerable<CourseViewModel>> GetAllAsync(PaginationParams @params);
