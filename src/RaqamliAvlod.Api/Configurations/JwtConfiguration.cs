777b7c988607ab695f32a88ea8a3dc875b044a58
SobirjonovSodiqjonovich <otkirbeksobirjonov@gmail.com>
2022-11-07 11:09:18 +0500

    Added and configured jwt config and swagger auth config

---
+++ src/RaqamliAvlod.Api/Configurations/JwtConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace RaqamliAvlod.Api.Configurations
+{
+    public static class JwtConfiguration
+    {
+        public static void ConfigureJwt(this WebApplicationBuilder builder)
+        {
+            var _config = builder.Configuration.GetSection("Jwt");
+            builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    options.TokenValidationParameters = new TokenValidationParameters()
+                    {
+                        ValidateIssuer = true,
+                        ValidIssuer = _config["Issuer"],
+                        ValidateAudience = false,
+                        ValidateIssuerSigningKey = true,
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Key"]))
+                    };
+                });
+        }
+    }
+}
