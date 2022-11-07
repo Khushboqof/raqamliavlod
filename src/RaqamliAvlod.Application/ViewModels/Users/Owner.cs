acd2dbcb7eea4799569e25f19b869ebd969b989b
Temur <ubaydullayev117@gmail.com>
2022-11-07 10:28:34 +0500

    updated

---
+++ src/RaqamliAvlod.Application/ViewModels/Users/Owner.cs
@@ -0,0 +1,24 @@
+﻿using RaqamliAvlod.Domain.Entities.Users;
+
+namespace RaqamliAvlod.Application.ViewModels.Users
+{
+    public class Owner
+    {
+        public long UserId { get; set; }
+
+        public string FullName { get; set; } = default!;
+
+        public string ImagePath { get; set; } = default!;
+
+
+        public static implicit operator Owner(User user)
+        {
+            return new Owner()
+            {
+                UserId = user.Id,
+                FullName = user.FirstName + " " + user.LastName,
+                ImagePath = user.ImagePath
+            };
+        }
+    }
+}
