8a389c8dc9abc04fd62bca206465954dfaf17179
Temur <ubaydullayev117@gmail.com>
2022-11-07 16:43:05 +0500

    Add new migration and models updated

---
+++ src/RaqamliAvlod.Infrastructure.Service/Interfaces/Courses/ICourseVideoService.cs
@@ -0,0 +1,19 @@
+﻿using RaqamliAvlod.Application.Utils;
+using RaqamliAvlod.Application.ViewModels.Courses;
+using RaqamliAvlod.Infrastructure.Service.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RaqamliAvlod.Infrastructure.Service.Interfaces.Courses;
+
+public interface ICourseVideoService
+{
+    Task<bool> CreateAsync(long ownerId, CourseVideoCreateDto dto);
+    Task<bool> UpdateAsync(long ownerId, long courseVideoId, CourseVideoUpdateDto dto);
+    Task<bool> DeleteAsync(long courseVideoId);
+    Task<IEnumerable<CourseVideoViewModel>> GetAllAsync(PaginationParams @params);
+
+}
