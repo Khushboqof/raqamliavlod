bf4e14e501581f9f0eeecb168293b0fdfe0f6d3d
SobirjonovSodiqjonovich <otkirbeksobirjonov@gmail.com>
2022-11-07 11:48:10 +0500

    updated auth

--- src/RaqamliAvlod.Api/Configurations/JwtConfiguration.cs
+++ src/RaqamliAvlod.Api/Configurations/JwtConfiguration.cs
@@ -17,7 +17,7 @@ namespace RaqamliAvlod.Api.Configurations
                         ValidateIssuer = true,
                         ValidIssuer = _config["Issuer"],
                         ValidateAudience = false,
-                        ValidateLifetime = true,
+                        //ValidateLifetime = true,
                         ValidateIssuerSigningKey = true,
                         IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Key"]))
                     };
