7d5bec163d75d577a5287f6c9abf88a68da22391
Temur <ubaydullayev117@gmail.com>
2022-11-08 08:44:52 +0500

    CourseVideoService added all methods, and new models create

--- src/RaqamliAvlod.Application/ViewModels/Courses/CourseVideoGetAllViewModel.cs
+++ src/RaqamliAvlod.Application/ViewModels/Courses/CourseVideoGetAllViewModel.cs
@@ -2,27 +2,23 @@
 
 namespace RaqamliAvlod.Application.ViewModels.Courses
 {
-    public class CourseVideoViewModel
+    public class CourseVideoGetAllViewModel
     {
         public long Id { get; set; }
         public string Title { get; set; } = string.Empty;
         public string YouTubeThumbnail { get; set; } = string.Empty;
         public int ViewCount { get; set; }
         public string YouTubeLink { get; set; } = string.Empty;
-        public string Description { get; set; } = string.Empty;
 
-        public string CourseName { get; set; } = string.Empty;
-
-        public static implicit operator CourseVideoViewModel(CourseVideo courseVideo)
+        public static implicit operator CourseVideoGetAllViewModel(CourseVideo courseVideo)
         {
-            return new CourseVideoViewModel()
+            return new CourseVideoGetAllViewModel()
             {
                 Id = courseVideo.Id,
                 Title = courseVideo.Title,
                 YouTubeThumbnail = courseVideo.YouTubeThumbnail,
                 ViewCount = courseVideo.ViewCount,
-                YouTubeLink = courseVideo.YouTubeLink,
-                Description = courseVideo.Description,
+                YouTubeLink = courseVideo.YouTubeLink
             };
         }
     }
