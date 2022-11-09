bf0be5bedea7982706ead8760228c0b6edc6bce3
Khushboqof <xushboqov.elbek@mail.ru>
2022-11-09 16:43:06 +0500

    bug fixed

--- src/RaqamliAvlod.Infrastructure.Service/Attributes/UsernameCheck.cs
+++ src/RaqamliAvlod.Infrastructure.Service/Attributes/UsernameCheck.cs
@@ -4,13 +4,22 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace RaqamliAvlod.Infrastructure.Service.Attributes
 {
+    [AttributeUsage(AttributeTargets.Property)]
     public class UsernameCheck : ValidationAttribute
     {
-        [AttributeUsage(AttributeTargets.Property)]
-         
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        {
+            var usernmae = (string)value!;
+
+            if (!usernmae.Contains('@'))
+                return ValidationResult.Success;
+
+            return new ValidationResult($"Cannot use '@' symbol");
+        }
     }
 }
