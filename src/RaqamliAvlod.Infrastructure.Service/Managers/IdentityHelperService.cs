fea4f9115ae7778b52846a2a9531352f43d79356
temurofficial07 <temuraslonboyev@gmail.com>
2022-11-10 17:13:47 +0500

    Updated v3

--- src/RaqamliAvlod.Infrastructure.Service/Managers/IdentityHelperService.cs
+++ src/RaqamliAvlod.Infrastructure.Service/Managers/IdentityHelperService.cs
@@ -15,28 +15,26 @@ namespace RaqamliAvlod.Infrastructure.Service.Managers
 
         public string GetUserEmail()
         {
-            return _accessor.HttpContext!.User.FindFirst("http://schemas.xmlsoap.org/ws/2005/05/identity/claims/emailaddress")!.Value;
+            var res = _accessor.HttpContext!.User.FindFirst("http://schemas.xmlsoap.org/ws/2005/05/identity/claims/emailaddress");
+            return res is not null ? res.Value : string.Empty;
         }
 
         public long? GetUserId()
         {
-            var res = _accessor.HttpContext!.User.FindFirst("Id")!.Value;
-            if (long.TryParse(res, out long id))
-                return id;
-            return null;
+            var res = _accessor.HttpContext!.User.FindFirst("Id");
+            return res is not null ? long.Parse(res.Value) : null; 
         }
 
         public string GetUserName()
         {
-            return _accessor.HttpContext!.User.FindFirst("http://schemas.xmlsoap.org/ws/2005/05/identity/claims/name")!.Value;
+            var res = _accessor.HttpContext!.User.FindFirst("http://schemas.xmlsoap.org/ws/2005/05/identity/claims/name");
+            return res is not null ? res.Value : string.Empty;
         }
 
         public UserRole? GetUserRole()
         {
-            var res = _accessor.HttpContext!.User.FindFirst("http://schemas.microsoft.com/ws/2008/06/identity/claims/role")!.Value;
-            if (Enum.TryParse<UserRole>(res, out UserRole role))
-                return role;
-            return null;
+            var res = _accessor.HttpContext!.User.FindFirst("http://schemas.microsoft.com/ws/2008/06/identity/claims/role");
+            return res is not null ? Enum.Parse<UserRole>(res.Value) : null;
         }
     }
 }
