bb9b571fd94b132b714c1648fa497a73e24eaf6f
temurofficial07 <temuraslonboyev@gmail.com>
2022-11-10 13:22:23 +0500

    Updated v2

--- src/RaqamliAvlod.Infrastructure.Service/Interfaces/Questions/IQuestionAnswerService.cs
+++ src/RaqamliAvlod.Infrastructure.Service/Interfaces/Questions/IQuestionAnswerService.cs
@@ -7,9 +7,9 @@ namespace RaqamliAvlod.Infrastructure.Service.Interfaces.Questions
 {
     public interface IQuestionAnswerService
     {
-        Task<bool> CreateAsync(QuestionAnswerCreateDto dto, long? userId);
-        Task<bool> UpdateAsync(long id, QuestionAnswerUpdateDto dto, long? userId);
-        Task<bool> DeleteAsync(long id, long? userId, UserRole? role);
+        Task<bool> CreateAsync(QuestionAnswerCreateDto dto, long userId);
+        Task<bool> UpdateAsync(long id, QuestionAnswerUpdateDto dto, long userId);
+        Task<bool> DeleteAsync(long id, long userId, UserRole role);
         Task<IEnumerable<QuestionAnswerViewModel>> GetAllAsync(long questionId, long? userId, PaginationParams? @params = null);
         Task<IEnumerable<QuestionAnswerViewModel>> GetRepliesAsync(long answerId, long? userId);
     }
