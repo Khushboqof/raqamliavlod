94506b97ea97939f698d374f6e0d00dbfd7c978e
Temur <ubaydullayev117@gmail.com>
2022-11-11 17:38:18 +0500

    New methods have been added to interfaces

--- src/RaqamliAvlod.DataAccess/Interfaces/Courses/ICourseRepository.cs
+++ src/RaqamliAvlod.DataAccess/Interfaces/Courses/ICourseRepository.cs
@@ -1,5 +1,6 @@
 ﻿using CodePower.DataAccess.Common;
 using RaqamliAvlod.Application.Utils;
+using RaqamliAvlod.Application.ViewModels.Courses;
 using RaqamliAvlod.Domain.Entities.Courses;
 
 namespace RaqamliAvlod.DataAccess.Interfaces.Courses
@@ -7,5 +8,8 @@ namespace RaqamliAvlod.DataAccess.Interfaces.Courses
     public interface ICourseRepository : IGenericRepository<Course>
     {
         Task<PagedList<Course>> SearchAsync(string text, PaginationParams @params);
+        Task<PagedList<CourseViewModel>> SearchByTitleAsync(string text, PaginationParams @params);
+        Task<PagedList<CourseViewModel>> GetAllViewAsync(PaginationParams @params);
+        Task<CourseViewModel?> GetViewAsync(long id);
     }
 }
