74561321f7be7d90fbb1095142a21a91ca625925
Kamronbek Maxamadzoidov <kamron25bek25@gmail.com>
2022-11-15 01:26:20 +0500

    PhoneNumberAttribute added

---
+++ src/RaqamliAvlod.Infrastructure.Service/Attributes/PhoneNumberAttribute.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
+
+namespace RaqamliAvlod.Infrastructure.Service.Attributes
+{
+    [AttributeUsage(AttributeTargets.Property)]
+    public class PhoneNumberAttribute : ValidationAttribute
+    {
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        {
+            string phoneNumber = (string)value!;
+            Regex regex = new Regex("^(?!0+$)(\\+\\d{1,3}[- ]?)?(?!0+$)\\d{10,15}$");
+
+            return regex.Match(phoneNumber).Success ? ValidationResult.Success
+                : new ValidationResult("Please enter valid phone number. Phone must be contains only numbers or + character");
+        }
+    }
+}
